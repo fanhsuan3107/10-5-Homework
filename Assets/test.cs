1fd0a6b39bbc789298061e5a14de5382b77a8ad2
fanhsuan3107 <fanhsuan3107@gmail.com>
2018-10-11 23:21:55 +0800

    Initial commit

---
+++ Assets/test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DateTime date = DateTime.Now;
+        GetComponent<TextMesh>().text = date.ToString();
+    }
+}
